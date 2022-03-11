784ef09eb6c54cf66e9c45ec6fe89f3df15446e4
Hugo C. Durães <hugocalheira@gmail.com>
2022-03-11 11:37:48 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import './app.css';
+
+/**
+ * React dotenv and custom environment variables
+ * https://create-react-app.dev/docs/adding-custom-environment-variables/
+ */
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  useEffect(() => {
+    console.log(process.env)
+  }, [])
+
+  return (<>
+    <h1>Environment (NODE_ENV)</h1>
+    <code>
+      {process.env.NODE_ENV} | {process.env.NODE_ENV === 'development' ? process.env.REACT_APP_dev : ''}
+      
+      {process.env.NODE_ENV === 'test' ? process.env.REACT_APP_homol : ''}
+
+      {process.env.NODE_ENV === 'production' ? process.env.REACT_APP_prod : ''}
+    </code>
+
+    <h2>.env file</h2>
+    <code>
+      {JSON.stringify(process.env)}
+    </code>
+
+    <h3>REACT_APP_dev</h3>
+    <code>{process.env.REACT_APP_dev}</code>
+
+    <h3>REACT_APP_homol</h3>
+    <code>{process.env.REACT_APP_homol}</code>
+
+    <h3>REACT_APP_prod</h3>
+    <code>{process.env.REACT_APP_prod}</code>
+
+    <h2>Temporary Environment Variables In Your Shell</h2>
+    <code>
+      REACT_APP_STAGE=123246549 npm start
+    </code>
+    <code>
+      {process.env.REACT_APP_STAGE}
+    </code>
+  </>
   );
 }
 
